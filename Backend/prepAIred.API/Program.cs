afbd10ed2f767c90e8a11e230cdd609290c1047b
Vasil Ganev <vgan3v@gmail.com>
2025-09-01 22:17:01 +0300

    feat(data): add data context

--- Backend/prepAIred.API/Program.cs
+++ Backend/prepAIred.API/Program.cs
@@ -1,9 +1,25 @@
+using prepAIred.Data;
+using DotNetEnv;
+using DotNetEnv.Configuration;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .AddDotNetEnv(".env", LoadOptions.TraversePath())
+    .Build();
+
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    string connectionString = builder.Configuration["Appsettings:DefaultConnection"]!;
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
