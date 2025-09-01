3e6e971950d14dd7f61e7ee0e116a65fed2b87f5
Vasil Ganev <vgan3v@gmail.com>
2025-09-01 19:37:10 +0300

    feat(backend): add starting api project

---
+++ Backend/prepAIred.API/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prepAIred.API
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        [Route("ping")]
+        public IActionResult Ping()
+        {
+            return Ok("Pong");
+        }
+    }
+}
