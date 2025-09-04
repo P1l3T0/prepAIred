c8aac4999b45d94daeda174e5b221a0ec664bb35
Vasil Ganev <vgan3v@gmail.com>
2025-09-04 16:46:09 +0300

    feat(dtos): add ai request and response dtos

---
+++ Backend/prepAIred.Data/DTOs/AI/AIRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace prepAIred.Data
+{
+    public class AIRequestDTO
+    {
+        public string AIAgent { get; set; } = string.Empty;
+        public string Field { get; set; } = string.Empty;
+        public string Level { get; set; } = string.Empty;
+        public int NumberOfQuestions { get; set; }
+    }
+}
---
+++ Backend/prepAIred.Data/DTOs/InterviewQuestions/InterviewQuestionsResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace prepAIred.Data
+{
+    public class InterviewQuestionsResponseDTO
+    {
+        public int ID { get; set; }
+        public string Question { get; set; } = string.Empty;
+        public List<string> Answers { get; set; } = new List<string>();
+    }
+}
