1402aabe97b253c6e36c7db415556754442b6a4d
Vasil Ganev <vgan3v@gmail.com>
2025-09-04 22:13:51 +0300

    refactor(models): change ai interview session to interview

---
+++ Backend/prepAIred.Data/Models/Interview.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace prepAIred.Data
+{
+    public class Interview : BaseModel
+    {
+        public int UserID { get; set; }
+        public string Question { get; set; } = string.Empty;
+        public string AnswersJson { get; set; } = "[]";
+
+        [NotMapped]
+        public List<string> Answers
+        {
+            get => JsonSerializer.Deserialize<List<string>>(AnswersJson) ?? new List<string>();
+            set => AnswersJson = JsonSerializer.Serialize(value);
+        }
+
+        [JsonIgnore]
+        public User? User { get; set; }
+
+        public override T ToDto<T>()
+        {
+            InterviewDTO interviewDTO = new InterviewDTO()
+            {
+                ID = ID,
+                DateCreated = DateCreated,
+                Question = Question,
+                Answers = Answers
+            };
+
+            return (T)(object)interviewDTO;
+        }
+    }
+}
