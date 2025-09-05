984d57bb31b9a93cdcc9497792e2411142fad4ec
Vasil Ganev <vgan3v@gmail.com>
2025-09-05 09:52:38 +0300

    feat(interview): add interview service

---
+++ Backend/prepAIred.Services/Services/Interview/InterviewService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using prepAIred.Data;
+
+namespace prepAIred.Services
+{
+    public class InterviewService(DataContext dataContext) : IInterviewService
+    {
+        private readonly DataContext _dataContext = dataContext;
+
+        public async Task CreateInterviewsAsync(List<Interview> interviews)
+        {
+            await _dataContext.Interviews.AddRangeAsync(interviews);
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public async Task<List<InterviewDTO>> GetInterviewsByUserIdAsync(int userId)
+        {
+            return await _dataContext.Interviews
+                .Where(i => i.UserID == userId && !i.IsAnswered)
+                .Select(i => i.ToDto<InterviewDTO>())
+                .ToListAsync();
+        }
+    }
+}
