fc64d219a80352b70ad7ac380f645cc2add2c952
Vasil Ganev <vgan3v@gmail.com>
2025-09-06 19:02:51 +0300

    refactor(interviewsession): adjust model and dto

--- Backend/prepAIred.Data/DTOs/Interview/InterviewSessionDTO.cs
+++ Backend/prepAIred.Data/DTOs/Interview/InterviewSessionDTO.cs
@@ -3,6 +3,8 @@
     internal class InterviewSessionDTO : BaseDTO
     {
         public int UserID { get; set; }
+        public AIAgent AIAgent { get; set; } = AIAgent.ChatGPT;
+        public InterviewSessionScore Score { get; set; } = InterviewSessionScore.NotRated;
         public List<InterviewDTO> Interviews { get; set; } = new List<InterviewDTO>();
     }
 }
--- Backend/prepAIred.Data/Models/Interview/InterviewSession.cs
+++ Backend/prepAIred.Data/Models/Interview/InterviewSession.cs
@@ -2,20 +2,21 @@
 {
     public class InterviewSession : BaseModel
     {
-        public int InterviewID { get; set; }
         public int UserID { get; set; }
-        public User? User { get; set; } 
+        public string Topic { get; set; } = string.Empty;
+        public AIAgent AIAgent { get; set; } = AIAgent.ChatGPT;
+        public InterviewSessionScore Score { get; set; } = InterviewSessionScore.Good;
+        public User? User { get; set; }
         public List<Interview> Interviews { get; set; } = new List<Interview>();
 
         public override T ToDto<T>()
         {
             InterviewSessionDTO interviewSessionDTO = new InterviewSessionDTO()
             {
-                ID = ID,
-                InterviewID = InterviewID,
                 UserID = UserID,
+                AIAgent = AIAgent,
+                Score = Score,
                 Interviews = Interviews.ConvertAll(interview => interview.ToDto<InterviewDTO>()),
-                DateCreated = DateCreated,
             };
 
             return (T)(object)interviewSessionDTO;
