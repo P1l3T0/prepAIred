4ebba690b71895c2d0bd910b096d8b9d69b9ef2f
Vasil Ganev <vgan3v@gmail.com>
2025-09-07 20:14:04 +0300

    feat(utils): add enums

---
+++ Frontend/src/Utils/enums.ts
@@ -0,0 +1,14 @@
+export enum AIAgent {
+  ChatGPT = 0,
+  Claude = 1,
+  Gemini = 2
+}
+
+export enum InterviewSessionScore {
+  Poor = 0,
+  Fair = 1,
+  Good = 2,
+  VeryGood = 3,
+  Excellent = 4,
+  NotRated = 5
+}
