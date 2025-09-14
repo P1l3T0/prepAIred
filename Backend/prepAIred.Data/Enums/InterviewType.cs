743af0423e6ef5692ef2950998efbb0a9316363d
Vasil Ganev <vgan3v@gmail.com>
2025-09-14 21:28:56 +0300

    feat(interview): add interview type enum

---
+++ Backend/prepAIred.Data/Enums/InterviewType.cs
@@ -0,0 +1,11 @@
+﻿public enum InterviewType
+{
+    /// <summary>
+    /// HR interview type focusing on soft skills.
+    /// </summary>
+    HR,
+    /// <summary>
+    /// Technical interview type focusing on technical skills.
+    /// </summary>
+    Technical
+}
