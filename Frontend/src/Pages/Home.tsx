83b8eee4445c728bc1032ae3d9976e678ae4375c
Vasil Ganev <vgan3v@gmail.com>
2025-11-04 20:45:47 +0200

    feat(theme): add dark/light theme-specific variables

--- Frontend/src/Pages/Home.tsx
+++ Frontend/src/Pages/Home.tsx
@@ -14,13 +14,14 @@ const Home = () => {
 
   return (
     <>
-      <h1>Welcome, {user?.username}!</h1>
-      <p>Email: {user?.email}</p>
-      <p>Member since: {user?.dateCreated.toLocaleDateString()}</p>
-      <p>User ID: {user?.id}</p>
+      <div className="bg-background">
+        <h1 className="text-text-primary">Welcome, {user?.username}!</h1>
+        <p className="text-text-primary">Email: {user?.email}</p>
+        <p className="text-text-primary">Member since: {user?.dateCreated.toLocaleDateString()}</p>
+        <p className="text-text-primary">User ID: {user?.id}</p>
+      </div>
 
       <ThemeButton />
-
       <LogOutButton />
 
       <br />
