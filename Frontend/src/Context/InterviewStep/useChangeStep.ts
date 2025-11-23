2d856be2deed0e8dc1efebee3abafaf7592c654e
Vasil Ganev <vgan3v@gmail.com>
2025-11-23 19:00:36 +0200

    refactor(stepper): extract change step logic into context and remove custom hook

---
+++ Frontend/src/Context/InterviewStep/useChangeStep.ts
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import { InterviewStepContext } from "./InterviewStepContext";
+
+export const useInterviewStep = () => {
+  const context = useContext(InterviewStepContext);
+  if (!context) throw new Error("useInterviewStep must be used within an InterviewStepProvider");
+
+    return context;
+};
